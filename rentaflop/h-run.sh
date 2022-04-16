fc7a8a2cf453582b8adebe33394352515efd330d
David Sokol <dasokol@umich.edu>
2022-04-15 22:59:19 -0400

    update hive files

--- rentaflop/h-run.sh
+++ rentaflop/h-run.sh
@@ -3,6 +3,12 @@
     echo -e "${RED}$CUSTOM_NAME is already running${NOCOLOR}" &&
     exit 1
 
+# check again 5 seconds later to make sure it's not just the middle of an update
+sleep 5
+[[ `ps aux | grep "daemon.py" | grep -v grep | wc -l` != 0 ]] &&
+    echo -e "${RED}$CUSTOM_NAME is already running${NOCOLOR}" &&
+    exit 1
+
 cd /hive/miners/custom/$CUSTOM_NAME/rentaflop-miner
 # if daemon log exists, it's been run before and reqs are installed so we run normally
 if [[ -f "daemon.log" ]]; then
